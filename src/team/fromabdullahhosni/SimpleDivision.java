d3146b1bc718af8384ab32aabf2996581647c3a5
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:43:07 +0400

    SimpleDivision added some explaining comments for SimpleDivision

--- src/team/fromabdullahhosni/SimpleDivision.java
+++ src/team/fromabdullahhosni/SimpleDivision.java
@@ -1,4 +1,16 @@
+/**
+ * SimpleDivision - Demonstrates integer division.
+ * 
+ * This class shows how to divide two integers and store the quotient.
+ * Note: Integer division truncates any decimal portion.
+ */
 public class SimpleDivision {
+    /**
+     * Main method that performs integer division.
+     * Divides total (100) by parts (5) to get the quotient.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         int total;
         int parts;
