822ea697748d3a12065bf3e64cef0eea50175734
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:35:23 +0400

    MultiplicationHeader added some explaining comments for MultiplicationHeader

--- src/team/fromabdullahhosni/MultiplicationHeader.java
+++ src/team/fromabdullahhosni/MultiplicationHeader.java
@@ -1,4 +1,16 @@
+/**
+ * MultiplicationHeader - Displays a formatted multiplication table header.
+ * 
+ * This class prints the header portion of a multiplication table,
+ * demonstrating the use of tab escape characters (\t) for alignment.
+ */
 public class MultiplicationHeader {
+    /**
+     * Main method that prints the multiplication table header.
+     * Uses tab escape characters (\t) to align numbers 1-10.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         System.out.println("=========================================");
         System.out.println("           MULTIPLICATION TABLE");
