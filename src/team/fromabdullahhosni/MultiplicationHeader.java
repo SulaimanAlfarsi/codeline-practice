8750641cf4177082985b323f133e72b9ed260014
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 08:58:21 +0400

    add title line

--- src/team/fromabdullahhosni/MultiplicationHeader.java
+++ src/team/fromabdullahhosni/MultiplicationHeader.java
@@ -1,5 +1,6 @@
 public class MultiplicationHeader {
     public static void main(String[] args) {
         System.out.println("=========================================");
+        System.out.println("           MULTIPLICATION TABLE");
     }
 }
