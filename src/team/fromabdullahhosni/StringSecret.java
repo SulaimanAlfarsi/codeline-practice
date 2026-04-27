cdfda82c65ad80166b19ac770def7829f7262fa2
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:02:40 +0400

    add length method call

--- src/team/fromabdullahhosni/StringSecret.java
+++ src/team/fromabdullahhosni/StringSecret.java
@@ -2,5 +2,6 @@ public class StringSecret {
     public static void main(String[] args) {
         String text;
         text = "Java Programming";
+        System.out.println(text.length());
     }
 }
