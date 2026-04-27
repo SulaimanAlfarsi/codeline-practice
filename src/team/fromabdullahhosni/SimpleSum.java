319bcb63452a14c07a38e034788eb76bf788e78a
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:41:38 +0400

    SimpleSum added some explaining comments for SimpleSum

--- src/team/fromabdullahhosni/SimpleSum.java
+++ src/team/fromabdullahhosni/SimpleSum.java
@@ -1,4 +1,16 @@
+/**
+ * SimpleSum - Demonstrates basic integer addition.
+ * 
+ * This class shows how to declare integer variables and perform
+ * simple arithmetic using the addition operator (+).
+ */
 public class SimpleSum {
+    /**
+     * Main method that calculates the sum of two integers.
+     * Declares num1 (15) and num2 (25), then calculates their sum.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         int num1;
         int num2;
