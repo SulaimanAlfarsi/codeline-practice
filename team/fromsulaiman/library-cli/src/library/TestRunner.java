ab625f206ff9318cdda47c4374c6ed420ffbc892
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-27 07:49:45 +0400

    add TestRunner to verify functionality

---
+++ team/fromsulaiman/library-cli/src/library/TestRunner.java
@@ -0,0 +1,16 @@
+package library;
+
+import java.util.Scanner;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.addBook(new Book("Java Basics", "John Doe"));
+        library.addBook(new Book("Learn Java", "Jane Smith"));
+        library.showAvailableBooks();
+        library.borrowBook("Java Basics");
+        library.showAvailableBooks();
+        library.returnBook("Java Basics");
+        library.showAvailableBooks();
+    }
+}
