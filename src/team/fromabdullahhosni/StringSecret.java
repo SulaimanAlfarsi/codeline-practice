4b950ba662957d43eaacb9f2efa878c2855cc6c1
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:39:11 +0400

    StringSecret added some explaining comments for StringSecret

--- src/team/fromabdullahhosni/StringSecret.java
+++ src/team/fromabdullahhosni/StringSecret.java
@@ -1,4 +1,17 @@
+/**
+ * StringSecret - Demonstrates finding the length of a string.
+ * 
+ * This class shows how to use the length() method to determine
+ * the number of characters in a String value.
+ */
 public class StringSecret {
+    /**
+     * Main method that calculates and prints string length.
+     * Declares a String variable and uses the length() method
+     * to find and display the number of characters.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         String text;
         text = "Java Programming";
