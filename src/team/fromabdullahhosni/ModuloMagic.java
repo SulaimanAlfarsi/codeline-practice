ae71bad86602e783394365b0a6527ab7a64e1a93
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:44:13 +0400

    ModuloMagic added some explaining comments for ModuloMagic

--- src/team/fromabdullahhosni/ModuloMagic.java
+++ src/team/fromabdullahhosni/ModuloMagic.java
@@ -1,4 +1,16 @@
+/**
+ * ModuloMagic - Demonstrates the modulo operator to find remainders.
+ * 
+ * This class shows how to use the modulo operator (%) to calculate
+ * the remainder after dividing one number by another.
+ */
 public class ModuloMagic {
+    /**
+     * Main method that calculates the remainder of division.
+     * Uses modulo operator (%) to find remainder of 17 divided by 3.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         int num1;
         int num2;
