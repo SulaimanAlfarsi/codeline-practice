3728ead187b9fd0e447779d81cf6bf1b665fe6d0
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 08:50:48 +0400

    add third step

--- src/team/fromabdullahhosni/StaircaseChallenge.java
+++ src/team/fromabdullahhosni/StaircaseChallenge.java
@@ -2,5 +2,6 @@ public class StaircaseChallenge {
     public static void main(String[] args) {
         System.out.println("#");
         System.out.println("##");
+        System.out.println("###");
     }
 }
