63c06ae9a88a21597e471f0625d24777646abc61
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:05:46 +0400

    add toUpperCase method

--- src/team/fromabdullahhosni/CaseChanger.java
+++ src/team/fromabdullahhosni/CaseChanger.java
@@ -2,5 +2,6 @@ public class CaseChanger {
     public static void main(String[] args) {
         String text;
         text = "Sprint One";
+        System.out.println(text.toUpperCase());
     }
 }
