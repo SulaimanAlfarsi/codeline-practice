d33f22c8b8ef152a0d6120a2b8a4773d4e427020
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:45:21 +0400

    NameWeaver added some explaining comments for NameWeaver

--- src/team/fromabdullahhosni/NameWeaver.java
+++ src/team/fromabdullahhosni/NameWeaver.java
@@ -1,4 +1,16 @@
+/**
+ * NameWeaver - Demonstrates string concatenation.
+ * 
+ * This class shows how to combine two strings using the + operator
+ * to create a full name with a space between the first and last name.
+ */
 public class NameWeaver {
+    /**
+     * Main method that concatenates first and last name.
+     * Uses the + operator to join firstName, a space, and lastName.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         String firstName;
         String lastName;
