e39501da7ac1bdbc4f6219f359be94db30a31da9
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:31:13 +0400

    FramedFocus added some explaining comments for FramedFocus

--- src/team/fromabdullahhosni/FramedFocus.java
+++ src/team/fromabdullahhosni/FramedFocus.java
@@ -1,4 +1,16 @@
+/**
+ * FramedFocus - Displays a message inside a star frame.
+ * 
+ * This class prints a rectangular frame made of asterisks (*)
+ * with the message "Keep Learning" centered inside.
+ */
 public class FramedFocus {
+    /**
+     * Main method that prints a framed message.
+     * Uses escape character \" to include quotes in output.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         System.out.println("***********************");
         System.out.println("* *");
