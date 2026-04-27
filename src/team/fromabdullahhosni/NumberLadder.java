797949a37aa91287db595954f295fb416dd67861
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:36:12 +0400

    NumberLadder added some explaining comments for NumberLadder

--- src/team/fromabdullahhosni/NumberLadder.java
+++ src/team/fromabdullahhosni/NumberLadder.java
@@ -1,4 +1,16 @@
+/**
+ * NumberLadder - Prints a climbing pattern of numbers.
+ * 
+ * This class demonstrates the use of tab escape characters (\t)
+ * to create a ladder-like number pattern from 1 to 5.
+ */
 public class NumberLadder {
+    /**
+     * Main method that prints a number ladder from 1 to 5.
+     * Uses tab escape characters (\t) to separate numbers on the same line.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("1\t2");
