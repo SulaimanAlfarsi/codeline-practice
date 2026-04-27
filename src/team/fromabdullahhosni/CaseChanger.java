2a2322c03412429732f206562c01688f54ade6e5
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:40:52 +0400

    CaseChanger added some explaining comments for CaseChanger

--- src/team/fromabdullahhosni/CaseChanger.java
+++ src/team/fromabdullahhosni/CaseChanger.java
@@ -1,4 +1,16 @@
+/**
+ * CaseChanger - Demonstrates converting strings to upper and lower case.
+ * 
+ * This class shows how to use toUpperCase() and toLowerCase() methods
+ * to transform the case of characters in a string.
+ */
 public class CaseChanger {
+    /**
+     * Main method that converts string to different cases.
+     * Declares a String and prints it in both uppercase and lowercase.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         String text;
         text = "Sprint One";
