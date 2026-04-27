974cb107d8b1d18efe833fec1c4d0701e50cdadb
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:21:55 +0400

    finished InitialImpression

--- src/team/fromabdullahhosni/InitialImpression.java
+++ src/team/fromabdullahhosni/InitialImpression.java
@@ -2,5 +2,6 @@ public class InitialImpression {
     public static void main(String[] args) {
         String fullName;
         fullName = "John Doe";
+        System.out.println(fullName.charAt(0));
     }
 }
