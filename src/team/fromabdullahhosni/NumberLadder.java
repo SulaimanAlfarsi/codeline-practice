6a0f1392ebfb472aff0539c0d1daf075780329fd
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 08:59:44 +0400

    add third line

--- src/team/fromabdullahhosni/NumberLadder.java
+++ src/team/fromabdullahhosni/NumberLadder.java
@@ -2,5 +2,6 @@ public class NumberLadder {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("1\t2");
+        System.out.println("1\t2\t3");
     }
 }
