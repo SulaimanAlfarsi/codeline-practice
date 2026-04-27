23aa6b43e4595adb10ad2b81e16d2062a1c24ce5
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-27 09:32:05 +0400

    StarMatrix added some explaining comments for StarMatrix

--- src/team/fromabdullahhosni/StarMatrix.java
+++ src/team/fromabdullahhosni/StarMatrix.java
@@ -1,4 +1,16 @@
+/**
+ * StarMatrix - Prints a 5x5 grid of asterisks.
+ * 
+ * This class demonstrates basic output by printing a grid pattern
+ * where each asterisk is separated by a single space.
+ */
 public class StarMatrix {
+    /**
+     * Main method that prints a 5x5 star grid.
+     * Each row contains 5 asterisks separated by spaces.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         System.out.println("* * * * *");
         System.out.println("* * * * *");
