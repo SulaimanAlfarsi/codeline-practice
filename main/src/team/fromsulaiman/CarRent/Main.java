2659be4bcdcd4709bae2027b94b3a38b3a08e9e4
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 12:00:47 +0400

    add menu loop

--- main/src/team/fromsulaiman/CarRent/Main.java
+++ main/src/team/fromsulaiman/CarRent/Main.java
@@ -6,5 +6,14 @@ public class Main {
     public static void main(String[] args) {
         RentalService service = new RentalService();
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Add Car");
+            System.out.println("2. View Cars");
+            System.out.println("3. Rent Car");
+            System.out.println("4. Return Car");
+            System.out.println("5. Exit");
+            System.out.print("Choose: ");
+        }
     }
 }
