8485cb1264080aebd69aee13f93d0debd2277efc
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 11:56:29 +0400

    add viewCars method

--- main/src/team/fromsulaiman/CarRent/RentalService.java
+++ main/src/team/fromsulaiman/CarRent/RentalService.java
@@ -1,4 +1,28 @@
 package team.fromsulaiman.CarRent;
+import java.util.ArrayList;
+
 
 public class RentalService {
+
+    private ArrayList<Car> cars = new ArrayList<>();
+
+    public void addCar(Car car) {
+        if (car == null) {
+            System.out.println("Invalid car.");
+            return;
+        }
+        cars.add(car);
+    }
+
+    public void viewCars() {
+        if (cars.isEmpty()) {
+            System.out.println("No cars available.");
+            return;
+        }
+
+        for (Car car : cars) {
+            car.displayInfo();
+        }
+    }
+
 }
