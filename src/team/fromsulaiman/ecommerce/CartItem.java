ea57c0470ca6f3ed55cfcf8ae3aabc63e8d8c666
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 09:39:21 +0400

    add constructor to CartItem

--- src/team/fromsulaiman/ecommerce/CartItem.java
+++ src/team/fromsulaiman/ecommerce/CartItem.java
@@ -3,4 +3,9 @@ package fromsulaiman.ecommerce;
 public class CartItem {
     private Product product;
     private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
 }
