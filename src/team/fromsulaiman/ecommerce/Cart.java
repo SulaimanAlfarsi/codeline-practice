df945ee041db1ea0eb997b9250fc93af4219fcce
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 09:41:43 +0400

    implement addItem method

--- src/team/fromsulaiman/ecommerce/Cart.java
+++ src/team/fromsulaiman/ecommerce/Cart.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 public class Cart {
     private ArrayList<CartItem> items = new ArrayList<>();
 
+    public void addItem(Product product, int quantity) {
+        items.add(new CartItem(product, quantity));
+    }
+
 }
