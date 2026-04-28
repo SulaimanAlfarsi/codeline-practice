ebd140d949d1588ccf56a311b1d30a69e55dc0af
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 14:20:35 +0400

    add RentalRecord data

--- main/src/team/fromsulaiman/CarRent/RentalRecord.java
+++ main/src/team/fromsulaiman/CarRent/RentalRecord.java
@@ -1,4 +1,15 @@
 package team.fromsulaiman.CarRent;
 
 public class RentalRecord {
+    private String carId;
+    private String customerName;
+
+    public RentalRecord(String carId, String customerName) {
+        this.carId = carId;
+        this.customerName = customerName;
+    }
+
+    public void display() {
+        System.out.println(carId + " rented by " + customerName);
+    }
 }
