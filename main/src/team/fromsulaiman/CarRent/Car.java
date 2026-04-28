406463fceee277438abc2ffbeecd69412a8b79e0
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-28 11:51:27 +0400

    add constructor to Car

--- main/src/team/fromsulaiman/CarRent/Car.java
+++ main/src/team/fromsulaiman/CarRent/Car.java
@@ -4,4 +4,11 @@ public class Car {
     private String id;
     private String model;
     private boolean isRented;
+
+    public Car(String id, String model) {
+        this.id = id;
+        this.model = model;
+        this.isRented = false;
+    }
+
 }
