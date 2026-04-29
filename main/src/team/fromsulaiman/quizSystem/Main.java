669339328cbc7965b18e9063fb6e45b67b44a894
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-29 08:43:35 +0400

    feat: add sample questions in Main

--- main/src/team/fromsulaiman/quizSystem/Main.java
+++ main/src/team/fromsulaiman/quizSystem/Main.java
@@ -1,9 +1,32 @@
 package team.fromsulaiman.quizSystem;
 
+import team.fromsulaiman.quizSystem.model.Question;
+import team.fromsulaiman.quizSystem.model.Quiz;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("=== Welcome to Quiz System ===");
 
+        Quiz quiz = new Quiz();
+
+        Question q1 = new Question(
+                "What is OOP?",
+                Arrays.asList("Object Oriented Programming", "Only One Program", "Open Online Project"),
+                "Object Oriented Programming"
+        );
+
+        Question q2 = new Question(
+                "Which keyword is used to create a class in Java?",
+                Arrays.asList("method", "class", "object"),
+                "class"
+        );
+
+        quiz.addQuestion(q1);
+        quiz.addQuestion(q2);
+
+        System.out.println("Quiz created with " + quiz.getQuestions().size() + " questions.");
     }
 }
