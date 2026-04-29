711325bbe724026ef8ce5c1e3c369ca63fea2913
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-29 08:33:18 +0400

    feat: add constructor to Question class

--- main/src/team/fromsulaiman/quizSystem/model/Question.java
+++ main/src/team/fromsulaiman/quizSystem/model/Question.java
@@ -4,4 +4,9 @@ public class Question {
     String questionText;
     String correctAnswer;
 
+    public Question(String questionText, String correctAnswer) {
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+    }
+
 }
