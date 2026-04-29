c95be03b9ad89c43d82a2c7d6a001f54a5e5aa1e
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-29 08:37:51 +0400

    feat: add question list to Quiz

--- main/src/team/fromsulaiman/quizSystem/model/Quiz.java
+++ main/src/team/fromsulaiman/quizSystem/model/Quiz.java
@@ -1,4 +1,17 @@
 package team.fromsulaiman.quizSystem.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz {
+
+    private List<Question> questions;
+
+    public Quiz() {
+        questions = new ArrayList<>();
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
 }
