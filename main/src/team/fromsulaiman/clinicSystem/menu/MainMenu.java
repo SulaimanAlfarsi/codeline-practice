683cdfe0c92bbe5ab360db87560ebd69fe8e3c7e
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-05-03 10:51:14 +0400

    feat: add main menu structure

---
+++ main/src/team/fromsulaiman/clinicSystem/menu/MainMenu.java
@@ -0,0 +1,50 @@
+package team.fromsulaiman.clinicSystem.menu;
+
+
+import team.fromsulaiman.clinicSystem.service.AppointmentService;
+import team.fromsulaiman.clinicSystem.service.DoctorService;
+import team.fromsulaiman.clinicSystem.service.PatientService;
+import team.fromsulaiman.clinicSystem.util.InputHelper;
+
+public class MainMenu {
+    private InputHelper inputHelper;
+    private DoctorService doctorService;
+    private PatientService patientService;
+    private AppointmentService appointmentService;
+
+    public MainMenu() {
+        inputHelper = new InputHelper();
+        doctorService = new DoctorService();
+        patientService = new PatientService();
+        appointmentService = new AppointmentService();
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("\n===== Clinic Appointment System =====");
+            System.out.println("1. Admin Menu");
+            System.out.println("2. Patient Menu");
+            System.out.println("3. Doctor Menu");
+            System.out.println("0. Exit");
+
+            int choice = inputHelper.readInt("Choose option: ");
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Admin menu coming soon...");
+                    break;
+                case 2:
+                    System.out.println("Patient menu coming soon...");
+                    break;
+                case 3:
+                    System.out.println("Doctor menu coming soon...");
+                    break;
+                case 0:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
