12161f00a1366f9652dc3f1dc19586f9674d0919
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-05-03 10:41:55 +0400

    feat: add abstract user model

---
+++ main/src/team/fromsulaiman/clinicSystem/model/User.java
@@ -0,0 +1,28 @@
+package team.fromsulaiman.clinicSystem.model;
+
+
+public abstract class User {
+    private String id;
+    private String name;
+    private String phone;
+
+    public User(String id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public abstract void displayInfo();
+}
