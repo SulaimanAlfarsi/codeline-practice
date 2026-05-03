29a77619dd26488589fa8044af728044dcacb351
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-05-03 10:38:13 +0400

    chore: initialize clinic appointment CLI project

---
+++ main/src/team/fromsulaiman/clinicSystem/Main.java
@@ -0,0 +1,7 @@
+package team.fromsulaiman.clinicSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Clinic Appointment System");
+    }
+}
